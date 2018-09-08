6c7df225d1c15f22854fec80aad00cf33c1c6632
nicholasc <nicain.seattle@gmail.com>
2018-09-08 12:20:18 -0700

    first commit

---
+++ dataframe_browser/dataframebrowser.py
@@ -0,0 +1,84 @@
+import networkx as nx
+import sys
+
+COMMAND_SEP_CHAR = ';'
+
+
+
+class TextController(object):
+
+    def __init__(self, **kwargs):
+        self.QUIT_VALS = kwargs.get('QUIT_VALS', ['q:'])
+
+    def parse_text_input(self, text_input):
+        return [input.strip() for input in text_input.split(COMMAND_SEP_CHAR)]
+
+
+    def parse_input(self, input):
+        if isinstance(input, (str,)):
+            input_list = self.parse_text_input(input)
+
+        elif isinstance(input, (list, tuple)):
+            input_list = input
+        
+        else:
+            raise NotImplementedError
+
+        return [self.input_mapper(input) for input in input_list]
+
+
+    def input_mapper(self, input):
+
+        if input in self.QUIT_VALS: 
+            return self.quit
+        else: 
+            return self.unrecognized
+
+
+    def quit(self, **kwargs): 
+        sys.exit(0)
+
+
+    def unrecognized(self, **kwargs):
+        print 'Unrecognized input: {0}'.format(kwargs['input_value'])
+
+    def update():
+        pass
+
+class TextControllerNonInteractive(TextController):
+
+    def update(self):
+        pass
+
+class Model(object):
+
+    def __init__(self, **kwargs):
+
+        self.graph = nx.DiGraph()
+
+
+class DataFrameBrowser(object):
+
+    def __init__(self, **kwargs):
+
+        model_kwargs = kwargs.get('model_kwargs', {})
+        self.model = Model(**model_kwargs)
+
+        controller_kwargs = kwargs.get('controller_kwargs', {})
+        self.controller = kwargs.get('controller_class', TextController)(**controller_kwargs)
+
+    def run(self, input=['']):
+
+        parsed_input_list = self.controller.parse_input(input)
+        while len(parsed_input_list) > 0:
+            curr_input = input.pop(0)
+            curr_input()
+            raise
+
+
+if __name__ == "__main__":
+
+    import pytest
+    pytest.main(['-s', '-x', '-v', '/home/nicholasc/projects/dataframe-browser'])
+
+    DataFrameBrowser().run(input=['', 'q:'])
---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from dataframe_browser.dataframebrowser import DataFrameBrowser
+import pandas as pd
+import pytest
+
+df = pd.DataFrame({'a':[1,2,3,1,2,2,2], 'b':[.1,.2,.3,.4, .5,.4,.1], 'c':['a', 'b', 'c', 'b', 'a', 'a', 'b']})
+
+def test_quit():
+
+    with pytest.raises(SystemExit) as pytest_wrapped_exception:
+        DataFrameBrowser().run(input=['q:'])
+    assert pytest_wrapped_exception.type == SystemExit
+    assert pytest_wrapped_exception.value.code == 0
+
+
+
+
+
