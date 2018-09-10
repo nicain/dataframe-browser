7a3d4b3685117b8d140a0d00a06f92e88cd31505
nicholasc <nicain.seattle@gmail.com>
2018-09-10 15:08:44 -0700

    WIP

---
+++ sandbox_bokeh.py
@@ -0,0 +1,16 @@
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html, components
+from bokeh.io import export_png
+
+plot = figure()
+plot.circle([1,2], [3,4])
+
+html = file_html(plot, CDN, "my plot")
+
+script, div = components(plot)
+
+# print html
+# print script, div
+
+export_png(plot, '~/tmp.png')
