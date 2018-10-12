5b448283426e73c525ab6aa68bcc842c9e763c6e
nicholasc <nicain.seattle@gmail.com>
2018-10-12 12:28:36 -0700

    Bump version: 0.1.0 → 0.2.0.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dataframe-browser',
-    version = '0.1.0',
+    version = 'version="0.2.0.dev0"',
     description = """Interactive pandas dataframe browser""",
     author = "Nicholas Cain",
     author_email = "nicain.seattle@gmail.com",
