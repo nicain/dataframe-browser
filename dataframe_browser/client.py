088824d41a97621b691b997d4d3dcb6994cee4f8
nicholasc <nicain.seattle@gmail.com>
2018-10-17 16:52:24 -0700

    use pickle to drive upload, so no worries about index_col

--- dataframe_browser/client.py
+++ dataframe_browser/client.py
@@ -97,11 +97,12 @@ class Cursor(object):
         import io
 
         buf = io.BytesIO()
-        df.to_csv(buf)
+        df.to_pickle(buf)
         buf.seek(0)
+        fname = 'file.p'
         upload_folder = requests.post(self.uri(base='upload_folder', include_session_uuid=False)).json()['upload_folder']
-        server_file_name = "{folder}/{file}".format(folder=upload_folder, file='file.csv') # dont use os.path because this is a path on the server not client
-        requests.post(self.uri(base='upload'), files={'file':('file.csv', buf)}, data={'filename':[server_file_name], 'command':['open']})
+        server_file_name = "{folder}/{file}".format(folder=upload_folder, file=fname) # dont use os.path because this is a path on the server not client
+        requests.post(self.uri(base='upload'), files={'file':(fname, buf)}, data={'filename':[server_file_name], 'command':['open']})
         self.reload()
 
     @property
