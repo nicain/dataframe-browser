8af98c738d13fbbd8678b64a5b42158fa7ab12a2
nicholasc <nicain.seattle@gmail.com>
2018-10-17 16:59:57 -0700

    cleaning out junk
    cleaning out junk 2
    
    cleaning out junk 3
    
    cleaning out junk 4

--- examples/read_sql_file.py
+++ examples/read_sql_file.py
@@ -14,18 +14,3 @@ filename2 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/qu
 
 c.read(uri='postgresql://limsreader@limsdb2:5432/lims2',
        password=pgpasslib.getpass('limsdb2', 5432, 'lims2', 'limsreader'), 
-       filename=[filename1, filename2])
-
-
-# c.open(filename='/home/nicholasc/projects/dataframe-browser/data/example.csv')
-# # c.query('a > 1')
-# # c.keep(['area', 'dsi_dg', 'osi_dg', 'g_dsi_dg',])
-# c.groupby('a')
-# c.bookmark(name='example')
-# c.drop(frames=[0])
-# c.back()
-# c.reload()
-
-# c.apply(column=['dsi_dg', 'g_dsi_dg', 'osi_dg'], mapper='dataframe_browser.mappers.widgets.xy_compare', new_column='bokeh', lazy=False)
-
-# c.apply(column=['dsi_dg', 'g_dsi_dg', 'osi_dg'], mapper='to_df', new_column='df', lazy=False, drop=True)
