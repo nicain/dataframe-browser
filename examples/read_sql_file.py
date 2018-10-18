c018386602cd94b56238ac34d07c0dcd91dcdef8
nicholasc <nicain.seattle@gmail.com>
2018-10-17 17:06:08 -0700

    updating examples for new default behavior
    update front end examples

--- examples/read_sql_file.py
+++ examples/read_sql_file.py
@@ -8,9 +8,10 @@ import allensdk.internal.core.lims_utilities as lu
 c = Cursor()
 
 
-# filename = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/processing_query.sql'
-filename1 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/container_pre_release_query.sql'
-filename2 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/experiment_pre_release_query.sql'
+filename1 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/processing_query.sql'
+filename2 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/container_pre_release_query.sql'
+filename3 = '/home/nicholasc/projects/allensdk_internal/allensdk/internal/api/queries/pre_release_sql/experiment_pre_release_query.sql'
 
 c.read(uri='postgresql://limsreader@limsdb2:5432/lims2',
        password=pgpasslib.getpass('limsdb2', 5432, 'lims2', 'limsreader'), 
+       filename=[filename1, filename2, filename3])
