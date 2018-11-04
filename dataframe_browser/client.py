159c913b4d72aeb857f5b6edf895dd9b02b0e8fe
nicholasc <nicain.seattle@gmail.com>
2018-11-04 13:00:36 -0800

    detect py3 on client instead of baking it in to class

--- dataframe_browser/client.py
+++ dataframe_browser/client.py
@@ -103,7 +103,7 @@ class Cursor(object):
         import dill
         import six
 
-        if self.__python_version__ == 3:
+        if six.PY3:
             raise RuntimeError('apply and update not supported in python3')
 
         if not isinstance(mapper, six.string_types):
@@ -197,12 +197,6 @@ if __name__ == "__main__":
     else:
         data_dict = {}
         
-    if six.PY3:
-        c.__python_version__ = 3
-        data_dict['python3'] = dill.dumps(c).decode("latin1") 
-    else: 
-        c.__python_version__ = 2
-        data_dict['python2'] = dill.dumps(c).decode("latin1") 
 
     json.dump(data_dict, open(save_file_name, 'w'))
 
