d8d48094e5c265290b2224fe19e5e9e7865c485a
nicholasc <nicain.seattle@gmail.com>
2018-11-04 18:23:34 -0800

    hinge class build from json, need post endpoint and then decoupled from main service

--- dataframe_browser/hinges/hinge_service.py
+++ dataframe_browser/hinges/hinge_service.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 from flask_cors import CORS
+from jinja2 import Environment, BaseLoader
 import hashlib
 
 app = Flask(__name__)
@@ -8,33 +9,43 @@ app.secret_key = 'super secret key'
 CORS(app, resources={r'/lazy_formatting/*':{"origins": "http://nicholasc-ubuntu:*"}}, allow_headers=['Content-Type'])
 
 
-class Hinge(object):
-
-    def __init__(self, name=None, uuid=None):
-        assert not name is None
-        self.name = name
-        self.button_html_list = []
-
-    def get_menu_html(self):
-        return '\n'.join(self.button_html_list)
-
-button_html = \
+button_template_str = \
 '''
 <form class="form-inline" action="/command/{session_uuid}/" method="POST">
-    <input type="hidden" name='columns' value='{column_string}'>
-    <input type="hidden" name='new_column' value='max_projection_image'>
-    <input type="hidden" name='drop' value='false'>
-    <input type="hidden" name='mapper' value='brain_observatory.nwb_file_to_max_projection'>
-    <input type="hidden" name='command' value="apply">
-    <button type="submit" class="w-100 btn btn-primary btn-sm mx-2 my-1">max_projection</button>
+    {% for name, value in hidden_name_value_dict.items() %}
+    <input type="hidden" name={{name}} value={{value}}>
+    {% endfor %}
+    <button type="submit" class="w-100 btn btn-primary btn-sm mx-2 my-1">{{title}}</button>
 </form>'''.strip()
 
+class Hinge(object):
+
+    button_template = Environment(loader=BaseLoader).from_string(button_template_str)
+    
+    def __init__(self, name=None, button_data_list=[]):
+        assert not name is None
+
+        self.name = name
+        self.button_data_list = [x for x in button_data_list]
+
+    def get_menu_html(self):
+        menu_html_list = []
+        for button_data in self.button_data_list:
+            menu_html_list.append(Hinge.button_template.render(**button_data))
+        return '\n'.join(menu_html_list)
+
+
+
+h_tmp = Hinge(name='BOb session', button_data_list=[{'title':'HW', 
+                                                     'hidden_name_value_dict':{'columns':"{column_string}",
+                                                                               "new_column":"max_projection_image",
+                                                                               "drop":"false",
+                                                                               "mapper":"brain_observatory.nwb_file_to_max_projection",
+                                                                               "command":"apply"}}])
+
+
 master_hinge_dict = {}
-
-h_tmp = Hinge(name='BOb session')
 master_hinge_dict['7fa00718647f4ebcaf42246eb36eb6b1'] = h_tmp
-
-h_tmp.button_html_list.append(button_html)
 
 @app.route('/<hinge_uuid>/', methods=['GET'])
 def hinge_html(hinge_uuid):
